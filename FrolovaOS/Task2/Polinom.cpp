982460a4ab944aa4d447bd430e3d91328500f703
Frolova Olga <Olka_frol@mail.ru>
2019-03-19 22:51:44 +0300

    @Frolova Task 2 Variant 2

--- FrolovaOS/Task2/Polinom.cpp
+++ FrolovaOS/Task2/Polinom.cpp
@@ -26,17 +26,18 @@ Polinom::Polinom(const Polinom &f)
 {
 	n = f.n;
 	Coef = new double[n + 1];
-	for (int i = 0; i <= n; i++)
+	for (int i = 0; i <= n+1; i++)
 		Coef[i] = f.Coef[i];
 }
 
 void Polinom::OutputPolinom()
 {
-	if (Coef[0] != 0)
+	cout << Coef[0]<<" ";
+	if (Coef[1] != 0)
 	{
-		cout << Coef[0];
+		cout << Coef[1];
 	}
-	for (int i = 1; i < n + 1; i++)
+	for (int i = 2; i <= n + 1; i++)
 	{
 		if (Coef[i] == 0)
 		{
@@ -45,11 +46,11 @@ void Polinom::OutputPolinom()
 		}
 		else if (Coef[i] > 0)
 		{
-			cout << " + " << Coef[i] << "X^" << i;
+			cout << " + " << Coef[i] << "X^" << i-1;
 		}
 		else if (Coef[i] < 0)
 		{
-			cout << " - " << Coef[i] << "X^" << i;
+			cout << " - " << Coef[i] << "X^" << i-1;
 		}
 
 	}
@@ -82,14 +83,14 @@ double& Polinom::operator [] (int ind)
 Polinom Polinom::derivative()
 {
 	int i;
-	Polinom Z(n - 1);
-	for (i = 0; i <= n - 1; i++)
+	Polinom Z(n-1 );
+	for (i = 1; i <= n ; i++)
 	{
-		Z.Coef[i] = (i + 1)*Coef[i + 1];
+		Z.Coef[i] = (i )*Coef[i +1];
 	}
 	return Z;
 }
-///////////////////////////////ERROW
+
 Polinom& Polinom::operator = (const Polinom &t)
 {
 	if (this != &t)
@@ -114,7 +115,7 @@ Polinom& Polinom::operator = (const Polinom &t)
 double Polinom::answer(double x)
 {
 	double ans = 0;
-	for (int i = 0; i < n + 1; i++)
+	for (int i = 1; i <= n + 1; i++)
 	{
 		ans = ans + (Coef[i] * pow(x, i));
 	}
@@ -122,12 +123,27 @@ double Polinom::answer(double x)
 }
 
 
-istream &operator>>(istream &s, Polinom &c)// перегруженный оператор ввода
+istream &operator>>(istream &s, Polinom &c)
 {
 	int i;
-	for (i = 0; i <= c.n; i++)
+	int k;
+	s >> k;
+	if (k != 0)
 	{
-		s >> c.Coef[i];
+		c.Coef[0] = k;
+		for (i = 1; i <= k+1; i++)
+			//for (i = 1; i < k + 1; i++)-------------- if we need number of coefficients
+		{
+			s >> c.Coef[i];
+		}
+	}
+	
+	else 
+	{
+		for (i = 0; i <= c.n + 1; i++)
+		{
+			s >> c.Coef[i];
+		}
 	}
 	return s;
 }
@@ -135,18 +151,21 @@ istream &operator>>(istream &s, Polinom &c)// перегруженный оператор ввода
 ostream &operator<<(ostream &s, const Polinom &c)
 {
 	int i, n = 0;
-	for (i = 0; i <= c.n; i++)
+	if (c.Coef[0] == 0)
 	{
-		if (c.Coef[i] != 0)
-			n++;
-	}
-	if (n != 0)
-	{
+		c.Coef[0] = c.n ;
+		//c.Coef[0] = c.n + 1; ---------- if we need number of coefficients
 
-		for (i = 0; i <= c.n; i++)
+		for (i = 0; i <= c.n + 1; i++)
 		{
 			s << c.Coef[i] << " ";
-
+		}
+	}
+	else 
+	{
+		for (i = 0; i <= c.n + 1; i++)
+		{
+			s << c.Coef[i] << " ";
 		}
 	}
 	return s;
